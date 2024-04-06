d84d76f1734fc0ebfdb8a9086c3c4365ccdf5dcc
Isaac-kiptoo-kilimo <isaackilimok2@gmail.com>
2024-04-06 08:17:11 +0300

    fix:fixed the images importation

--- src/pages/aboutKenya/AboutKenyaCarousel.jsx
+++ src/pages/aboutKenya/AboutKenyaCarousel.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import aboutKenyaImg1 from "./assets/about-kenya.png";
 import aboutKenyaImg2 from "./assets/about-kenya-two.png";
-import aboutKenyaImg3 from "./assets/about-kenya-three.jpg";
+import aboutKenyaImg3 from "./assets/about-kenya3.jpg";
 import aboutKenyaImg4 from "./assets/about-kenya4.jpg";
 import aboutKenyaImg5 from "./assets/about-kenya-five.jpg";
 import aboutKenyaImg10 from "./assets/about-kenya10.jpg";
